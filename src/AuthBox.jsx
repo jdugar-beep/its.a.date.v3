348fe03444b78e65ce1d865b6d3355fdb41f1b78
jdugar-beep <jayveer.dugar@gmail.com>
2026-05-14 19:48:38 -0500

    Update AuthBox.jsx

--- src/AuthBox.jsx
+++ src/AuthBox.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { supabase } from "./utils/supabase";
+import { supabase } from "./utils/supabase.js";
 
 export default function AuthBox({ user, setUser }) {
   const [email, setEmail] = useState("");
